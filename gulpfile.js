5d75cef9f44e2bef7fd5fb2ef2e354df7b3adb1a
Lamson Le <ll217@u.washington.edu>
2015-07-14 14:23:34 -0700

    clean

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,9 @@ var jshint = require('gulp-jshint');
 
 gulp.task('lint', function() {
   var options = {node: true};
-  return gulp.src(['./gulpfile.js', './build/*.js'])
+  return gulp.src(['./gulpfile.js', './js/*.js'])
     .pipe(jshint(options))
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', function() {
-
-});
+gulp.task('default', ['lint']);
